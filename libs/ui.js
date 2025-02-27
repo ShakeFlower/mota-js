11575c683ac40740e3c457bacdef04e6fd022bf9
ShakeFlower <theshakeflower@Gmail.com>
2025-02-27 09:03:41 +0800

    fix:高清画面

--- libs/ui.js
+++ libs/ui.js
@@ -2578,8 +2578,7 @@ ui.prototype.drawFly = function (page) {
     }
     var size = this.PIXEL - 143;
     core.strokeRect('ui', 20, 100, size, size, '#FFFFFF', 2);
-    core.drawThumbnail(floorId, null, { ctx: 'ui', x: 20, y: 100, size: size, damage: true, drawSize: 0.6 });
-    // 好像是无法理解的魔数，有待将来研究及优化
+    core.drawThumbnail(floorId, null, { ctx: 'ui', x: 20, y: 100, size: size, damage: true });
 }
 
 ////// 绘制中心对称飞行器
@@ -3094,7 +3093,6 @@ ui.prototype._drawSLPanel_drawRecord = function (title, data, x, y, size, cho, h
         core.drawThumbnail(data.floorId, map.blocks, {
             heroLoc: data.hero.loc, heroIcon: data.hero.image, flags: data.hero.flags,
             ctx: 'ui', x: x - size / 2, y: y + 15, size: size, centerX: data.hero.loc.x, centerY: data.hero.loc.y, noHD: true,
-            drawSize: 0.285, // 完全无法理解的魔数
         });
         if (core.isPlaying() && core.getFlag("hard") != data.hero.flags.hard) {
             core.fillRect('ui', x - size / 2, y + 15, size, size, [0, 0, 0, 0.4]);
